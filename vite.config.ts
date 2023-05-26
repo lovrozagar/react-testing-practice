d341dce1f91ed7448bb62b5fe35e3e30cdbd7881
lovro <lovro.zagar5@gmail.com>
2023-05-26 23:40:31 +0200

    build: initial commit, setup react + ts + tailwind + vitest

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest"/>
+/// <reference types="vite/client"/>
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  test: {
+    css: true,
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  },
+})
